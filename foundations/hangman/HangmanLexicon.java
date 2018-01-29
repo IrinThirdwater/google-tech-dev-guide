c093cf634cbbb8b8cdffea12bd6563601b194d95
Jirapat Treyanurak <34905030+IrinThirdwater@users.noreply.github.com>
2018-01-29 14:08:57 +0000

    Update HangmanLexicon.java
    Keep the provided list as a default constructor and add an additional constructor for lexicon based on a text file.

--- foundations/hangman/HangmanLexicon.java
+++ foundations/hangman/HangmanLexicon.java
@@ -1,11 +1,21 @@
 package hangman;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * This class contains a stub implementation of the HangmanLexicon class.
+ * This class is a lexicon for the hangman game.
+ * Either create a new lexicon with a default list of words
+ * or provide the file name for a text file to read the words from.
  */
 public class HangmanLexicon {
 
-    private String[] words = {
+    private String[] defaultWords = {
             "BUOY",
             "COMPUTER",
             "CONNOISSEUR",
@@ -17,6 +27,45 @@ public class HangmanLexicon {
             "SLITHER",
             "ZIRCON"
     };
+    private List<String> words;
+    private String lexiconSource;
+
+    /**
+     * Create a hangman lexicon with a default list of words.
+     */
+    public HangmanLexicon () {
+        words = Arrays.<String>asList(defaultWords);
+    }
+
+    /**
+     * Create a hangman lexicon with words from the source file.
+     *
+     * @param source Source file for lexicon words.
+     */
+    public HangmanLexicon (String source) throws IOException {
+        words = new ArrayList<String>();
+        lexiconSource = source;
+        parseSource();
+    }
+
+    /**
+     * Parse the source file and populate the lexicon words with its content.
+     */
+    private void parseSource () throws IOException {
+        String path = Paths.get(".")
+                .toAbsolutePath()
+                .normalize()
+                .toString();
+        path += "/" + lexiconSource;
+        // the try-with-resources idiom for SE7 or later
+        try (BufferedReader br =
+                new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line.toLowerCase());
+            }
+        }
+    }
 
     /**
      * Returns the number of words in the lexicon.
@@ -24,7 +73,7 @@ public class HangmanLexicon {
      * @return The number of words in the lexicon.
      */
     public int getWordCount () {
-        return words.length;
+        return words.size();
     }
 
     /**
@@ -34,7 +83,7 @@ public class HangmanLexicon {
      * @return The word in the lexicon corresponding to the given index.
      */
     public String getWord (int index) {
-        return words[index];
+        return words.get(index);
     }
 
 }
